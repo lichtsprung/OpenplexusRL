15e44d16cc36ecc8abc154f203e0f2d1c8d3c7c8
Robert Giacinto <robert.giacinto@gmail.com>
2012-05-15 18:19:56 +0200

    new hexagons

--- src/main/scala/net/openplexus/World.scala
+++ src/main/scala/net/openplexus/World.scala
@@ -1,7 +1,6 @@
 package net.openplexus
 
 import org.newdawn.slick.Graphics
-import collection.mutable.Buffer
 
 
 /**
@@ -24,19 +23,12 @@ class World() {
  */
 case class Cell(val id: Int) {
   var cellType: String = ""
-
-  var north: Cell = null
   var northeast: Cell = null
   var northwest: Cell = null
   var west: Cell = null
   var east: Cell = null
-  var south: Cell = null
   var southwest: Cell = null
   var southeast: Cell = null
-
-
-
-  private val entities = Buffer[Entity]()
   var visited: Boolean = false
   var visible: Boolean = false
   var marked: Boolean = false
@@ -60,12 +52,6 @@ case class Cell(val id: Int) {
 
   }
 
-  def registerNeighborSouth(neighbor: Cell) {
-    if (neighbor != null) {
-      south = neighbor
-      neighbor.north = this
-    }
-  }
 
   def registerNeighborSoutheast(neighbor: Cell) {
     if (neighbor != null) {
@@ -83,13 +69,6 @@ case class Cell(val id: Int) {
 
   }
 
-  def registerNeighborNorth(neighbor: Cell) {
-    if (neighbor != null) {
-      north = neighbor
-      neighbor.south = this
-    }
-
-  }
 
   def registerNeighborNortheast(neighbor: Cell) {
     if (neighbor != null) {
@@ -108,20 +87,6 @@ case class Cell(val id: Int) {
   }
 
 
-  /**
-   * Registers an entity with this cell.
-   * @param entity the new entity
-   * @return a list of all entities on this cell
-   */
-  def registerEntity(entity: Entity) = entities += entity
-
-  /**
-   * Removes an entity from this cell
-   * @param entity the entity to be removed
-   * @return a list of all entities on this cell
-   */
-  def unregisterEntity(entity: Entity) = entities -= entity
-
   def draw(g: Graphics) = {
 
   }
@@ -137,13 +102,21 @@ case class Cell(val id: Int) {
 case class WorldMap(val player: Player) {
   private val idGenerator = new IDGenerator()
   private val seedCell = Cell(idGenerator.nextID())
-  private var mark = false
+  private var mark = true
 
   init()
-  resetMarkers()
+
 
   player.setPosition(seedCell)
 
+
+  def createEvenRow(cell: Cell, width: Int): Unit = {
+
+  }
+
+  def createUnevenRow(cell: Cell, width: Int): Unit = {
+
+  }
 
   /**
    * Initializes the map.
@@ -151,122 +124,75 @@ case class WorldMap(val player: Player) {
    * @param height the starting height of the map
    */
   def init(width: Int = 5, height: Int = 5) = {
-    var top = seedCell
+    // create top row
+//    var current = seedCell
+//    for (i <- 1 to width) {
+//      current.registerNeighborEast(Cell(idGenerator.nextID()))
+//      current = current.east
+//    }
+//
+//    current = seedCell
+//    createUnevenRow(current, width)
 
-    // Oberste Zeile der Map erzeugen.
-    for (i <- 0 to width) {
-      val topEast = Cell(idGenerator.nextID())
-      top.registerNeighborEast(topEast)
-      top = topEast
-    }
+    //    for (i<- 1 to height){
+    //      if (i % 2 == 0){
+    //        createEvenRow(current)
+    //      }else{
+    //        createUnevenRow(current)
+    //      }
+    //    }
 
-    var bottom = Cell(idGenerator.nextID())
-    var nextTop = bottom
-    top = seedCell
 
-    for (k <- 1 to height) {
 
-      for (i <- 1 to width) {
-        val topEast = top.east
-        val bottomEast = Cell(idGenerator.nextID())
 
-        top.registerNeighborSoutheast(bottomEast)
-        top.registerNeighborSouth(bottom)
-
-        bottom.registerNeighborEast(bottomEast)
-        bottom.registerNeighborNortheast(topEast)
-
-        top = topEast
-        bottom = bottomEast
-
-      }
-      top = nextTop
-      bottom = Cell(idGenerator.nextID())
-      nextTop = bottom
-    }
-  }
-
-  /**
-   * Traverses the underlying graph and prints some information about the cells
-   */
-  def traversMap(): Unit = {
-    visit(seedCell)
-    resetMarkers()
-  }
-
-  def resetMarkers(): Unit = reset(seedCell)
-
-  private def reset(cell: Cell):Unit = {
-    if (cell != null && cell.marked) {
-      cell.marked = false
-      reset(cell.north)
-      reset(cell.northwest)
-      reset(cell.west)
-      reset(cell.southwest)
-      reset(cell.south)
-      reset(cell.southeast)
-      reset(cell.east)
-      reset(cell.northeast)
-    }
-  }
-
-  private def visit(cell: Cell): Unit = {
-    if (cell != null && !cell.marked) {
-      //      printf("Cell %s is connected to \n", cell)
-      //      printf("\tNorth: Cell %s\n", cell.north)
-      //      printf("\tNorthwest: Cell %s\n", cell.northwest)
-      //      printf("\tWest: Cell %s\n", cell.west)
-      //      printf("\tSouthwest: Cell %s\n", cell.southwest)
-      //      printf("\tSouth: Cell %s\n", cell.south)
-      //      printf("\tSoutheast: Cell %s\n", cell.southeast)
-      //      printf("\tEast: Cell %s\n", cell.east)
-      //      printf("\tNortheast: Cell %s\n", cell.northeast)
-      cell.marked = true
-      visit(cell.north)
-      visit(cell.northwest)
-      visit(cell.west)
-      visit(cell.southwest)
-      visit(cell.south)
-      visit(cell.southeast)
-      visit(cell.east)
-      visit(cell.northeast)
-    }
+    seedCell.registerNeighborNortheast(Cell(idGenerator.nextID()))
+//    seedCell.registerNeighborEast(Cell(idGenerator.nextID()))
+//    seedCell.registerNeighborSoutheast(Cell(idGenerator.nextID()))
+//    seedCell.registerNeighborSouthwest(Cell(idGenerator.nextID()))
+//    seedCell.registerNeighborWest(Cell(idGenerator.nextID()))
+//    seedCell.registerNeighborNorthwest(Cell(idGenerator.nextID()))
   }
 
 
-  def draw(g: Graphics) = {
-    drawCell(seedCell, 0, 0)
+  def drawCell(cell: Cell): Unit = {
+    drawCell(cell, 0, 0)
     mark = !mark
   }
 
-  private def drawCell(cell: Cell, indexX: Int, indexY: Int): Unit = {
-    if (cell != null && mark == cell.marked) {
-      Game.sprites.getEmptyCell().draw(indexX * Game.sprites.getSpriteWidth(), indexY * Game.sprites.getSpriteHeight())
-      cell.marked = true
-      drawCell(cell.north, indexX, indexY - 1)
-      drawCell(cell.northwest, indexX - 1, indexY - 1)
-      drawCell(cell.west, indexX - 1, indexY)
-      drawCell(cell.southwest, indexX - 1, indexY + 1)
-      drawCell(cell.south, indexX, indexY + 1)
-      drawCell(cell.southeast, indexX + 1, indexY + 1)
-      drawCell(cell.east, indexX + 1, indexY)
-      drawCell(cell.northeast, indexX + 1, indexY + 1)
-    } else {
-      Game.sprites.getBlockedCell().draw(indexX * Game.sprites.getSpriteWidth(), indexY * Game.sprites.getSpriteHeight())
+  def drawCell(cell: Cell, x: Int, y: Int): Unit = {
+    if (cell != null && cell.marked != mark) {
+      val width = x * Game.sprites.getSpriteWidth().toFloat
+      val height = y * (Game.sprites.getSpriteHeight()).toFloat
+      Game.sprites.getEmptyCell().draw(width, height)
+      cell.marked = !cell.marked
+      drawDiagonalCell(cell.northeast, x + 1, y - 1)
+      drawCell(cell.east, x + 1, y)
+      drawDiagonalCell(cell.southeast, x + 1, y + 1)
+      drawDiagonalCell(cell.southwest, x - 1, y + 1)
+      drawCell(cell.west, x - 1, y)
+      drawDiagonalCell(cell.northwest, x - 1, y - 1)
     }
   }
 
-  def movePlayerNorth() = {
-    if (player.getPosition.north != null) {
-      player.setPosition(player.getPosition.north)
+  def drawDiagonalCell(cell: Cell, x: Int, y: Int): Unit = {
+    if (cell != null && cell.marked != mark) {
+      val width = x * Game.sprites.getSpriteWidth() - (Game.sprites.getSpriteWidth() / 2).toFloat
+      val height = y * (Game.sprites.getSpriteHeight() - 8).toFloat
+      Game.sprites.getEmptyCell().draw(width, height)
+      cell.marked = !cell.marked
+      drawDiagonalCell(cell.northeast, x + 1, y - 1)
+      drawCell(cell.east, x + 1, y)
+      drawDiagonalCell(cell.southeast, x + 1, y + 1)
+      drawDiagonalCell(cell.southwest, x - 1, y + 1)
+      drawCell(cell.west, x - 1, y)
+      drawDiagonalCell(cell.northwest, x - 1, y - 1)
     }
   }
 
-  def movePlayerSouth() = {
-    if (player.getPosition.south != null) {
-      player.setPosition(player.getPosition.south)
-    }
+  def draw(g: Graphics) = {
+    drawCell(seedCell)
   }
+
 
   // TODO fertigstellen
 }
